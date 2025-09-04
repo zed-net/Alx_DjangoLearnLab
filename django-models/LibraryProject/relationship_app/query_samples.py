7ef631bb98f8bb2ac84ce25d731680633f7108d1
zed <zedekjacob@gmail.com>
2025-09-04 18:47:33 +0100

    new2

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,15 @@
 >>> from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
->>> author = Author.objects.get(name="J.K. Rowling")
+>>> asimov = Author.objects.get(name="Isaac Asimov")
+>>> asimov_books = asimov.books.all()
 
 # List all books in a library
 
->>> books_in_library = Library.objects.get(name=library_name)
+>>> alexandria = Library.objects.get(name="Alexandria Library")
+>>> library_books = alexandria.books.all()
 
 
 # Retrieve  the librarian of a library
->>> librarian = library.librarianS
+>>> alexandria = Library.objects.get(name="Alexandria Library")
+>>> librarian = alexandria.librarian
