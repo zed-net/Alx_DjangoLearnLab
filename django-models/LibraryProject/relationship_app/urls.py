bd96b7e3dc72a7be8847dd5b2549e256f582954c
zed <zedekjacob@gmail.com>
2025-09-05 21:41:59 +0100

    new1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,9 +4,12 @@ from .views import list_books
 
 urlpatterns = [
     # URL for the function-based view to list all books
-    path('books/', views.book_list, name='book-list'),
+    path('books/', views.list_books, name='book-list'),
 
     # URL for the class-based view to show a specific library's details
     # The <int:pk> part captures the primary key of the library from the URL.
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+     path('register/', views.register, name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render , redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Book
-
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
-def book_list(request):
+
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
@@ -20,4 +23,27 @@ class LibraryDetailView(DetailView):
     # The name of the variable that will hold the library object in the template.
     context_object_name = 'library'
     
+    
+def register(request):
+    
+    if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('/')
+    else:
+            form = UserCreationForm()
+    return render(request, 'relationship_app/registration/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    
+    
+    template_name = 'relationship_app/registration/login.html'
+
+class CustomLogoutView(LogoutView):
+    
+
+    template_name = 'relationship_app/registration/logout.html'
+
 
