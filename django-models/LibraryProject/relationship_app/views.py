4247f66d5b993e40469dee00ce53f54ef119db85
zed <zedekjacob@gmail.com>
2025-09-05 21:17:49 +0100

    new4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import Book
-from django.views.generic import DetailView
+
+from django.views.generic.detail import DetailView
 from .models import Library
 
 def book_list(request):
