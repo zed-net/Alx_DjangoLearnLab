a994193c898bc21a0f74a91867616ae425a85d81
zed <zedekjacob@gmail.com>
2025-09-06 14:45:58 +0100

    new2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -65,19 +65,19 @@ def is_member(user):
 
 # --- Role-Based Views ---
 @user_passes_test(is_admin)
-def admin_view(request):
+def AdminView(request):
     """A view accessible only to Admin users."""
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
-def librarian_view(request):
+def LibrarianView(request):
     """A view accessible only to Librarian users."""
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
-def member_view(request):
+def MemberView(request):
     """A view accessible only to Member users."""
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
 
 # --- Access Denied View ---
 def not_allowed(request):
