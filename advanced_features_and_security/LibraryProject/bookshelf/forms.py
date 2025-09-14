77ad784ef7f3536b511912549ea57387d0833f66
zed <zedekjacob@gmail.com>
2025-09-14 12:53:05 +0100

    new2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -8,3 +8,10 @@ class BookForm(forms.ModelForm):
 
 class BookSearchForm(forms.Form):
     q = forms.CharField(max_length=200, required=False)
+    
+  
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label="Your Name")
+    email = forms.EmailField(required=True, label="Your Email")
+
