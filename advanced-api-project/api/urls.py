a7d7739ab58ca279b7bce5f3b8fdc05cbc2f6bd5
zed <zedekjacob@gmail.com>
2025-09-28 20:10:06 +0100

    api project4

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
     path('books/create/', CreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
+    path('books/update/', UpdateView.as_view(), name='book-update'),
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),
     
     
 ]
