527cc55878eeec5bde5de96a1665537b8fdd3cc4
zed <zedekjacob@gmail.com>
2025-10-01 17:01:04 +0100

    blog api

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'myusername',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',  
+        'PORT': '5432',   
     }
 }
 
