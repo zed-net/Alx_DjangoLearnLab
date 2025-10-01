8e911ef2211353d3aeac8a88b23bf86699490211
zed <zedekjacob@gmail.com>
2025-10-01 17:42:40 +0100

    blog api

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 path('posts/', PostListView.as_view(), name='post-list'),
 path('posts/new/', PostCreateView.as_view(), name='post-create'),
 path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
