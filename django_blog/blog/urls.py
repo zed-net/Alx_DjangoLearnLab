795fbf212969c42c773b501a0bc5acf3313ab91b
zed <zedekjacob@gmail.com>
2025-10-03 15:28:15 +0100

    comment2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,4 +18,8 @@ path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), nam
 path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-edit'),
 path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 
+
+path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
