36d1868a187b06a3915bcf40e262b17c509f5c3b
zed <zedekjacob@gmail.com>
2025-10-03 16:58:21 +0100

    comment2

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-    
+    tags = TaggableManager(blank=True)    
     def __str__(self):
         return self.title
     
