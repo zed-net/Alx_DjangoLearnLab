ab78a96aeb7cbf8735f25a9812f81e984da71298
zed <zedekjacob@gmail.com>
2025-10-10 11:07:45 +0100

    social api 3

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,19 +5,27 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    """
+    Serializer for registering new users.
+    Automatically creates a token upon registration.
+    """
+    # Write-only field means it’s accepted during creation but never returned in API response
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
+       
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+
+        # Create authentication token for the new user
         Token.objects.create(user=user)
         return user
