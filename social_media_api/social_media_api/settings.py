e903bf0b91f81ab2cbc095a1a7da6e695300eadb
zed <zedekjacob@gmail.com>
2025-10-10 17:35:54 +0100

    social api22

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -169,3 +169,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
+PORT = int(os.environ.get("PORT", 8000))
